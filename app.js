3f2539701b0c35664a72082f5a3d5a4b352cc31e
implements <implements@users.noreply.github.com>
2025-04-05 13:27:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ app.controller('TravelPlansController', function($scope) {
 
     $scope.locations = [
         {
-            title: 'Do a Coast to Coast Road Trip in a Cadillac.',
+            title: 'Coast to Coast Road Trip in a Cadillac.',
             country: 'United States',
             url: 'https://en.wikipedia.org/wiki/Road_trip',
             year: 2009
+        },
+        {
+            title: 'Backpack Through Asia.',
+            country: 'Vietnam / South Korea / Japan',
+            url: 'https://en.wikipedia.org/wiki/Backpacking_(travel)',
+            year: 2010
+        },     
+        {
+            title: 'Go on a Interrail Trip',
+            country: 'Netherlands / France / Spain / Italy / Germany / Czech Republic',
+            url: 'https://en.wikipedia.org/wiki/Interrail',
+            year: 2010
         },
         
     ]
