ff1bf18bb507892a3a22909dadcb647877013a2f
implements <implements@users.noreply.github.com>
2025-04-05 14:48:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -62,12 +62,6 @@ app.controller('TravelPlansController', function($scope) {
             country: 'Taiwan',
             url: 'https://en.wikipedia.org/wiki/Suhua_Highway',
             year: 2016
-        }, 
-        {
-            title: 'Swim With Sea Lions.',
-            country: 'Peru',
-            url: 'https://en.wikipedia.org/wiki/Lima',
-            year: 2016
         },
         {
             title: 'Climb a Volcano.',
